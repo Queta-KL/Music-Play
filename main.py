4695949ab5d52d15a9ace95cd7e3a463bc4672e0
Kuida Liu <51280897+Queta-KL@users.noreply.github.com>
2020-01-30 16:38:55 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import music
 
 def main():
-	haha=music.MusicalNotation(r"InTheSpring.txt")
-	haha.nmn2MusicPlayer().write(r"InTheSpring.wav")
+	haha=music.MusicalNotation(r"Freude.txt")
+	haha.nmn2MusicPlayer().write(r"Freude.wav")
 
 if __name__ == '__main__':
 	main()
