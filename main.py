b079ed846351e88e183ccc86b821c88f45da6ca0
Kuida Liu <51280897+Queta-KL@users.noreply.github.com>
2020-02-04 16:09:57 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import music
+import numpy as np
+import pydub
 
 def main():
-	haha=music.MusicalNotation(r"Freude.txt")
-	haha.nmn2MusicPlayer().write(r"Freude.wav")
+	scoreList=("Freude",
+		"InTheSpring",
+		"TheMoonOfLuzhou")
+	inputFile=".\\MusicScore\\"
+	outputFile=".\\output\\"
+	for score in scoreList:
+		haha=music.MusicalNotation(inputFile+score+r".txt")
+		haha.nmn2MusicPlayer().write(outputFile+score+r".wav")
+		sound=pydub.AudioSegment.from_wav(outputFile+score+r".wav")
+		sound.export(outputFile+score+r".mp3", format="mp3")
+		print(score,"OK")
+	return
 
 if __name__ == '__main__':
 	main()
